3a330835077238384f45748c1642b0623b649187
uken49 <heldergavioes@hotmail.com>
2023-05-15 13:59:52 -0300

    first commit

---
+++ src/main/java/com/example/apianalisecredito/ApiAnaliseCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apianalisecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnaliseCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnaliseCreditoApplication.class, args);
+	}
+
+}
