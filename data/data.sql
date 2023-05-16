2ebd6d3afce6ef7cac7f562a6fe73a7dc29de158
uken49 <heldergavioes@hotmail.com>
2023-05-16 14:34:26 -0300

    Removendo duplicidade do data.sql

--- data/data.sql
+++ data/data.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS CREDITY_ANALYSIS (
     id UUID PRIMARY KEY,
     approved BOOLEAN,
     approved_limit DECIMAL(10, 2) NOT NULL,
-    requested_amount DECIMAL(10, 2) NOT NULL,
     withdraw DECIMAL(10, 2) NOT NULL,
     monthly_income DECIMAL(10, 2) NOT NULL,
     requested_amount DECIMAL(10, 2) NOT NULL,
