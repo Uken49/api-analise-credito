c9e347dd329f321c9c838114c0cf592b2d8dd4b0
uken49 <heldergavioes@hotmail.com>
2023-05-16 10:03:46 -0300

    Adicionando ResponseStatus e criando metodo Controller: add ResponseStatus e bean da service; Service: criando metodo requestCreditAnalysis

--- src/main/java/com/example/apianalisecredito/controller/CreditAnalysisController.java
+++ src/main/java/com/example/apianalisecredito/controller/CreditAnalysisController.java
@@ -2,31 +2,42 @@ package com.example.apianalisecredito.controller;
 
 import com.example.apianalisecredito.controller.request.AnalysisCreditRequest;
 import com.example.apianalisecredito.controller.response.AnalysisCreditResponse;
+import com.example.apianalisecredito.service.CreditAnalysisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/credit/analysis")
+@RequiredArgsConstructor
 public class CreditAnalysisController {
 
+    private final CreditAnalysisService service;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public AnalysisCreditResponse requestCreditAnalysis(
-            @RequestBody AnalysisCreditRequest analysisCreditRequest
+            @RequestBody AnalysisCreditRequest dataToAnalyze
     ){
-        return null;
+        return service.requestCreditAnalysis(dataToAnalyze);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public AnalysisCreditResponse listCreditAnalysis(){
         return null;
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public AnalysisCreditResponse getCreditAnalysisById(@PathVariable UUID id){
         return null;
     }
+
     @GetMapping("/{cpf}")
+    @ResponseStatus(HttpStatus.OK)
     public AnalysisCreditResponse getCreditAnalysisByCpf(@PathVariable Integer cpf){
         return null;
     }
--- src/main/java/com/example/apianalisecredito/service/CreditAnalysisService.java
+++ src/main/java/com/example/apianalisecredito/service/CreditAnalysisService.java
@@ -1,4 +1,13 @@
 package com.example.apianalisecredito.service;
 
+import com.example.apianalisecredito.controller.request.AnalysisCreditRequest;
+import com.example.apianalisecredito.controller.response.AnalysisCreditResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CreditAnalysisService {
+    public AnalysisCreditResponse requestCreditAnalysis(AnalysisCreditRequest dataToAnalyze) {
+
+        return null;
+    }
 }
