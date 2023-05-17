9f275edb52b43371ad0317c3b964ee69b1ed6a04
uken49 <heldergavioes@hotmail.com>
2023-05-17 15:24:08 -0300

    Criando os métodos do mapper

--- src/main/java/com/example/apianalisecredito/mapper/CreditAnalysisMapper.java
+++ src/main/java/com/example/apianalisecredito/mapper/CreditAnalysisMapper.java
@@ -1,8 +1,18 @@
 package com.example.apianalisecredito.mapper;
 
+import com.example.apianalisecredito.controller.request.CreditAnalysisRequest;
+import com.example.apianalisecredito.controller.response.CreditAnalysisResponse;
+import com.example.apianalisecredito.model.CreditAnalysisModel;
+import com.example.apianalisecredito.repository.entity.CreditAnalysisEntity;
 import org.mapstruct.Mapper;
 
-@Mapper
-public class CreditAnalysisMapper {
+@Mapper(componentModel = "spring")
+public interface CreditAnalysisMapper {
+
+    CreditAnalysisModel fromModel(CreditAnalysisRequest creditAnalysisRequest);
+
+    CreditAnalysisEntity fromEntity(CreditAnalysisModel creditAnalysisModel);
+
+    CreditAnalysisResponse fromRequest(CreditAnalysisEntity creditAnalysisEntity);
 
 }
