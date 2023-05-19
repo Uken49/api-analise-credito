e35d2ebab54fe0026dcf2d7cb67cc5eb13e797c2
uken49 <heldergavioes@hotmail.com>
2023-05-19 11:30:06 -0300

    Removendo NOT NULL dos atributos

--- data/data.sql
+++ data/data.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS CREDITY_ANALYSIS (
     id UUID PRIMARY KEY,
     approved BOOLEAN,
-    approved_limit DECIMAL(10, 2) NOT NULL,
-    withdraw DECIMAL(10, 2) NOT NULL,
-    monthly_income DECIMAL(10, 2) NOT NULL,
-    requested_amount DECIMAL(10, 2) NOT NULL,
-    annual_interest DECIMAL(5, 2) NOT NULL,
+    approved_limit DECIMAL(10, 2),
+    withdraw DECIMAL(10, 2),
+    monthly_income DECIMAL(10, 2),
+    requested_amount DECIMAL(10, 2),
+    annual_interest DECIMAL(5, 2),
     client_id UUID NOT NULL,
     date DATE
 );
