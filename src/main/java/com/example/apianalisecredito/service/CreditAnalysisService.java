f798f4a2b9834bc1545651e8abb5b846a5d8080e
uken49 <heldergavioes@hotmail.com>
2023-05-22 15:51:04 -0300

    remove: cadastro por cpf

--- src/main/java/com/example/apianalisecredito/service/CreditAnalysisService.java
+++ src/main/java/com/example/apianalisecredito/service/CreditAnalysisService.java
@@ -9,7 +9,7 @@ import com.example.apianalisecredito.model.CreditAnalysisModel;
 import com.example.apianalisecredito.repository.CreditAnalysisRepository;
 import com.example.apianalisecredito.repository.entity.CreditAnalysisEntity;
 import java.math.BigDecimal;
-import java.util.Objects;
+import java.math.RoundingMode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,34 +23,28 @@ public class CreditAnalysisService {
 
     // Business rules
     private final BigDecimal withdrawalLimitPercentage = BigDecimal.valueOf(0.1);
-    private final BigDecimal interestPerYearPercentage = BigDecimal.valueOf(0.15);
+    private final BigDecimal interestPerYearPercentage = new BigDecimal(15);
     private final BigDecimal maxAmountOfMonthlyIncomeConsidered = BigDecimal.valueOf(50_000.00);
     private final BigDecimal ifRequestedValueIsGreaterThan50 = BigDecimal.valueOf(0.15);
     private final BigDecimal ifTheRequestedValueIsLessThanOrEqual50 = BigDecimal.valueOf(0.3);
     private final BigDecimal percentageForCreditAnalysis = BigDecimal.valueOf(0.5);
     private final int equalToHalfTheValue = 0;
 
-
     public CreditAnalysisResponse requestCreditAnalysis(CreditAnalysisRequest creditAnalysisRequest) {
 
         final CreditAnalysisModel creditAnalysisModel = mapper.fromModel(creditAnalysisRequest);
-        final String idOrCpf;
+        final ApiClientDto clientById = apiClient.getClientByIdOrCpf(creditAnalysisModel.clientId().toString());
 
-        if (Objects.isNull(creditAnalysisModel.cpf())) {
-            idOrCpf = creditAnalysisModel.clientId().toString();
-        } else {
-            idOrCpf = creditAnalysisModel.cpf();
-        }
-
-        final ApiClientDto clientById = apiClient.getClientByIdOrCpf(idOrCpf);
         final CreditAnalysisModel creditAnalysisModelUpdated;
+        final CreditAnalysisEntity creditAnalysisEntity;
+        final CreditAnalysisEntity creditAnalysisEntitySaved;
 
-        final BigDecimal requestedAmount = creditAnalysisModel.requestedAmount();
-        final BigDecimal monthlyIncome = creditAnalysisModel.monthlyIncome();
+        final BigDecimal requestedAmount = creditAnalysisModel.requestedAmount().setScale(2, RoundingMode.HALF_UP);
+        final BigDecimal monthlyIncome = creditAnalysisModel.monthlyIncome().setScale(2, RoundingMode.HALF_UP);
 
         final BigDecimal consideredValue;
 
-        final Boolean approved;
+        final boolean approved;
         final BigDecimal approvedLimit;
         final BigDecimal withdraw;
         final BigDecimal annualInterest;
@@ -60,34 +54,30 @@ public class CreditAnalysisService {
             withdraw = BigDecimal.valueOf(0);
             annualInterest = BigDecimal.valueOf(0);
             approvedLimit = BigDecimal.valueOf(0);
-            creditAnalysisModelUpdated = creditAnalysisModel.creditAnalysisUpdate(approved, approvedLimit, withdraw, annualInterest);
-            return null; //exception
-        }
-
-        if (requestedAmount.compareTo(monthlyIncome.multiply(maxAmountOfMonthlyIncomeConsidered)) > equalToHalfTheValue) {
-            consideredValue = maxAmountOfMonthlyIncomeConsidered;
         } else {
-            consideredValue = requestedAmount;
-        }
 
-        if (requestedAmount.multiply(percentageForCreditAnalysis).compareTo(monthlyIncome) > equalToHalfTheValue) {
-            approvedLimit = consideredValue.multiply(ifRequestedValueIsGreaterThan50);
-        } else {
-            approvedLimit = consideredValue.multiply(ifTheRequestedValueIsLessThanOrEqual50);
-        }
+            if (requestedAmount.compareTo(monthlyIncome.multiply(maxAmountOfMonthlyIncomeConsidered)) > equalToHalfTheValue) {
+                consideredValue = maxAmountOfMonthlyIncomeConsidered;
+            } else {
+                consideredValue = requestedAmount;
+            }
 
-        approved = true;
-        withdraw = approvedLimit.multiply(withdrawalLimitPercentage);
-        annualInterest = interestPerYearPercentage;
+            if (requestedAmount.multiply(percentageForCreditAnalysis).compareTo(monthlyIncome) > equalToHalfTheValue) {
+                approvedLimit = consideredValue.multiply(ifRequestedValueIsGreaterThan50);
+            } else {
+                approvedLimit = consideredValue.multiply(ifTheRequestedValueIsLessThanOrEqual50);
+            }
+
+            approved = true;
+            withdraw = approvedLimit.multiply(withdrawalLimitPercentage);
+            annualInterest = interestPerYearPercentage;
+        }
 
         creditAnalysisModelUpdated = creditAnalysisModel.creditAnalysisUpdate(approved, approvedLimit, withdraw, annualInterest);
-        System.out.println(creditAnalysisModelUpdated);
+        creditAnalysisEntity = mapper.fromEntity(creditAnalysisModelUpdated);
+        creditAnalysisEntitySaved = repository.save(creditAnalysisEntity);
 
-        final CreditAnalysisEntity creditAnalysisEntity = mapper.fromEntity(creditAnalysisModelUpdated);
-        final CreditAnalysisResponse creditAnalysisResponse = mapper.fromResponse(creditAnalysisEntity);
-
-        System.out.println("chegou ao fim");
-        System.out.println(creditAnalysisModelUpdated);
-        return creditAnalysisResponse;
+        return mapper.fromResponse(creditAnalysisEntitySaved);
     }
+
 }
