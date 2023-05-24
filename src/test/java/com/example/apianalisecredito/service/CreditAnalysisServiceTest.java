ee937aae0f726b23f653396073412bd61bf57e62
uken49 <heldergavioes@hotmail.com>
2023-05-24 12:52:07 -0300

    add: teste se api não retornar usuário

--- src/test/java/com/example/apianalisecredito/service/CreditAnalysisServiceTest.java
+++ src/test/java/com/example/apianalisecredito/service/CreditAnalysisServiceTest.java
@@ -1,4 +1,5 @@
 package com.example.apianalisecredito.service;
+
 import static com.example.apianalisecredito.factory.CreditAnalysisFactory.apiClientDtoFactory;
 import static com.example.apianalisecredito.factory.CreditAnalysisFactory.creditAnalysisRequestFactory;
 import static com.example.apianalisecredito.factory.CreditAnalysisFactory.entityNotApprovedFactory;
@@ -6,17 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 import com.example.apianalisecredito.apiclient.ApiClient;
+import com.example.apianalisecredito.apiclient.dto.ApiClientDto;
 import com.example.apianalisecredito.controller.request.CreditAnalysisRequest;
 import com.example.apianalisecredito.controller.response.CreditAnalysisResponse;
 import com.example.apianalisecredito.handler.exception.ClientNotFoundException;
-import com.example.apianalisecredito.handler.exception.CreditAnalysisNotFoundException;
 import com.example.apianalisecredito.mapper.CreditAnalysisMapper;
 import com.example.apianalisecredito.mapper.CreditAnalysisMapperImpl;
 import com.example.apianalisecredito.repository.CreditAnalysisRepository;
 import com.example.apianalisecredito.repository.entity.CreditAnalysisEntity;
+import feign.FeignException;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -34,6 +36,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CreditAnalysisServiceTest {
 
+    @Captor
+    ArgumentCaptor<CreditAnalysisEntity> creditAnalysisEntityArgumentCaptor;
     @Mock
     private CreditAnalysisRepository repository;
     @Mock
@@ -44,14 +48,14 @@ class CreditAnalysisServiceTest {
     private CreditAnalysisService service;
     @Captor
     private ArgumentCaptor<String> idArgumentCaptor;
-    @Captor ArgumentCaptor<CreditAnalysisEntity> creditAnalysisEntityArgumentCaptor;
 
     @Test
     void should_refuse_credit_analysis_with_requestetAmount_greater_than_monthlyIncome() {
         final CreditAnalysisRequest creditAnalysisRequest = creditAnalysisRequestFactory();
         final CreditAnalysisEntity creditAnalysisEntity = entityNotApprovedFactory();
+        final ApiClientDto apiClientDto = apiClientDtoFactory();
 
-        when(apiClient.getClientByIdOrCpf(idArgumentCaptor.capture())).thenReturn(apiClientDtoFactory());
+        when(apiClient.getClientByIdOrCpf(idArgumentCaptor.capture())).thenReturn(apiClientDto);
         when(repository.save(creditAnalysisEntityArgumentCaptor.capture())).thenReturn(creditAnalysisEntity);
 
         final CreditAnalysisResponse creditAnalysisResponse = service.requestCreditAnalysis(creditAnalysisRequest);
@@ -71,16 +75,12 @@ class CreditAnalysisServiceTest {
     }
 
     @Test
-    @Disabled("Working")
-    void deve_retornar_exception_quando_idCliente_nao_for_encontrado(){
+    void should_return_exception_when_idCliente_is_not_found() {
         final CreditAnalysisRequest creditAnalysisRequest = creditAnalysisRequestFactory();
 
-        when(apiClient.getClientByIdOrCpf(idArgumentCaptor.capture()))
-                .thenThrow(
-                        new ClientNotFoundException("Cliente com id: %s não foi encontrado"
-                                .formatted("234c443e-5853-4830-8f1b-dd55ca65c48a"))
-                );
+        when(apiClient.getClientByIdOrCpf(idArgumentCaptor.capture())).thenThrow(FeignException.class);
 
-        assertThrows(ClientNotFoundException.class, () -> service.requestCreditAnalysis(creditAnalysisRequest));
+        assertThrows(ClientNotFoundException.class,
+                () -> service.requestCreditAnalysis(creditAnalysisRequest));
     }
 }
